3258cc9967da1d298064c1cd67ff2273259c77d8
fandiyana <38607425+fandiyana@users.noreply.github.com>
2025-06-05 10:36:23 +0700

    Add .next/static/chunks/src_app_profile_page_tsx_a993527f._.js 1749094947626

---
+++ .next/static/chunks/src_app_profile_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_31e8950c._.js",
+  "static/chunks/src_771dfb3c._.js"
+],
+    source: "dynamic"
+});
